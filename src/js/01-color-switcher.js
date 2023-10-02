f632d802ca7bedaf23fe29f8257447e5e5549f03
Andrii Bilyk <bilykaim@gmail.com>
2023-10-02 17:51:06 +0300

    updates task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
+  
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  const body = document.body;
+  
+  let intervalId;
+  
+  function changeBackgroundColor() {
+    body.style.backgroundColor = getRandomHexColor();
+  }
+  
+  function handleButtonClick(event) {
+    if (event.target === startButton) {
+      startButton.disabled = true;
+      stopButton.disabled = false;
+      intervalId = setInterval(changeBackgroundColor, 1000);
+    } else if (event.target === stopButton) {
+      startButton.disabled = false;
+      stopButton.disabled = true;
+      clearInterval(intervalId);
+    }
+  }
+  
+  startButton.addEventListener('click', handleButtonClick);
+  stopButton.addEventListener('click', handleButtonClick);
